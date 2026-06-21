fdb50331d8d20c9ae3df4bcd27c80b087bfcf65d
manosgonidakis <manosgonidakis93@gmail.com>
2026-06-21 18:07:53 +0300

    Update GenRescue.cpp

--- src/pGenRescue/GenRescue.cpp
+++ src/pGenRescue/GenRescue.cpp
@@ -2,17 +2,19 @@
 #include <cmath>
 #include "MBUtils.h"
 #include "XYSegList.h" 
-#include "GenRescue.h" // Αλλαγή σε GenRescue
+#include "GenRescue.h"
 
 using namespace std;
 
 // Constructor
 GenRescue::GenRescue()
 {
-  // Αφαιρέσαμε το m_got_last_point γιατί δεν υπάρχουν πια firstpoint/lastpoint
   m_nav_x = 0;
   m_nav_y = 0;
+  m_enemy_x = 0; // Αρχικοποίηση θέσης αντιπάλου
+  m_enemy_y = 0;
   m_path_update_needed = false;
+  m_first_run_done = false;
 }
 
 // Destructor
@@ -26,7 +28,6 @@ bool GenRescue::OnConnectToServer()
   return(true);
 }
 
-
 bool GenRescue::OnNewMail(MOOSMSG_LIST &NewMail)
 {
   AppCastingMOOSApp::OnNewMail(NewMail);
@@ -36,7 +37,7 @@ bool GenRescue::OnNewMail(MOOSMSG_LIST &NewMail)
     CMOOSMsg &msg = *p;
     string key = msg.GetKey();
 
-    // 1. Ενημέρωση της τρέχουσας θέσης του σκάφους
+    // 1. Ενημέρωση της δικής μας θέσης
     if(key == "NAV_X") {
       m_nav_x = msg.GetDouble();
     }
@@ -44,7 +45,19 @@ bool GenRescue::OnNewMail(MOOSMSG_LIST &NewMail)
       m_nav_y = msg.GetDouble();
     }
     
-    // 2. Λήψη νέου κολυμβητή από τον Διαιτητή (uFldRescueMgr)
+    // 📌 ΝΕΟ: Ενημέρωση της θέσης του αντιπάλου (είτε παίζεις με το 'ben' είπε με το 'abe')
+    else if(key == "NAV_X_BEN" || key == "NAV_X_ABE") {
+      if(m_host_community != msg.GetCommunity()) { // Σιγουρευόμαστε ότι είναι ο άλλος
+        m_enemy_x = msg.GetDouble();
+      }
+    }
+    else if(key == "NAV_Y_BEN" || key == "NAV_Y_ABE") {
+      if(m_host_community != msg.GetCommunity()) {
+        m_enemy_y = msg.GetDouble();
+      }
+    }
+    
+    // 2. Λήψη νέου κολυμβητή από τον Διαιτητή (uFldRescueMgr) [cite: 156, 184]
     else if(key == "SWIMMER_ALERT" && msg.IsString()) {
       string sval = msg.GetString(); 
 
@@ -52,20 +65,16 @@ bool GenRescue::OnNewMail(MOOSMSG_LIST &NewMail)
       double y = 0;
       string id = "";
 
-      // Διαχωρισμός του string με βάση το κόμμα
       vector<string> svector = parseString(sval, ',');
       for(unsigned int i=0; i<svector.size(); i++) {
         string param = biteStringX(svector[i], '=');
         string value = svector[i];
         
-        // ΝΕΟ ΚΟΜΜΑΤΙ: Καθαρισμός από κενά (spaces) δεξιά και αριστερά
         param = stripBlankEnds(param);
         value = stripBlankEnds(value);
         
         if(param == "x") x = atof(value.c_str());
         else if(param == "y") y = atof(value.c_str());
-        
-        // Δέχεται και 'id' και 'name'
         else if(param == "id" || param == "name") id = value; 
       }
 
@@ -76,11 +85,11 @@ bool GenRescue::OnNewMail(MOOSMSG_LIST &NewMail)
           XYPoint new_swimmer(x, y);
           new_swimmer.set_label(id);
           m_swimmers[id] = new_swimmer;
-          m_path_update_needed = true; //
+          m_path_update_needed = true; 
       }
     }
     
-    // 3. Ειδοποίηση ότι ένας κολυμβητής βρέθηκε/διασώθηκε
+    // 3. Ειδοποίηση ότι ένας κολυμβητής διασώθηκε [cite: 349]
     else if(key == "FOUND_SWIMMER" && msg.IsString()) {
       string sval = msg.GetString(); 
       string id = "";
@@ -94,13 +103,8 @@ bool GenRescue::OnNewMail(MOOSMSG_LIST &NewMail)
         param = stripBlankEnds(param);
         value = stripBlankEnds(value);
         
-        // 📌 ΔΙΟΡΘΩΘΗΚΕ: Εδώ ξεκινάει με σκέτο "if" και το επόμενο είναι "else if"
-        if(param == "id" || param == "name") {
-          id = value;
-        }
-        else if(param == "finder") {
-          finder = value;
-        }
+        if(param == "id" || param == "name") id = value;
+        else if(param == "finder") finder = value;
       }
 
       if (m_swimmers.find(id) != m_swimmers.end()) {
@@ -113,6 +117,7 @@ bool GenRescue::OnNewMail(MOOSMSG_LIST &NewMail)
   }
   return(true);
 }
+
 bool GenRescue::Iterate()
 {
   AppCastingMOOSApp::Iterate();
@@ -122,22 +127,22 @@ bool GenRescue::Iterate()
     return(true);
   }
 
-  // --- 1. ΕΛΕΓΧΟΣ ΚΑΙ ΑΠΟΣΤΟΛΗ RESCUE_REQUEST (Τρέχει πάντα) ---
+  // --- 1. ΕΛΕΓΧΟΣ ΚΑΙ ΑΠΟΣΤΟΛΗ RESCUE_REQUEST --- [cite: 185]
   for(auto const& swimmer_pair : m_swimmers) {
     std::string id = swimmer_pair.first;
     XYPoint point = swimmer_pair.second;
     
     double dist = hypot(point.x() - m_nav_x, point.y() - m_nav_y);
 
-    if(dist <= 5.0) {
+    if(dist <= 5.0) { // [cite: 186]
       string request = "vname=" + m_host_community + ",name=" + id;
-      Notify("RESCUE_REQUEST", request);
+      Notify("RESCUE_REQUEST", request); // [cite: 185]
     }
   }
 
-  // --- 2. ΕΝΗΜΕΡΩΣΗ ΔΙΑΔΡΟΜΗΣ (Τρέχει ΜΟΝΟ όταν υπάρχει αλλαγή) ---
-  static bool first_run_done = false;
-  if (m_path_update_needed || (!first_run_done && !m_swimmers.empty())) {
+  // --- 2. ΕΝΗΜΕΡΩΣΗ ΔΙΑΔΡΟΜΗΣ (Με Διορθώσεις Claude Code) ---
+  // Αντικαταστήσαμε το static με το μέλος m_first_run_done
+  if (m_path_update_needed || (!m_first_run_done && !m_swimmers.empty())) {
       
       XYSegList my_path; 
       double current_x = m_nav_x;
@@ -145,7 +150,6 @@ bool GenRescue::Iterate()
 
       map<string, XYPoint> remaining_swimmers = m_swimmers;
 
-      // Greedy Αλγόριθμος
       while(!remaining_swimmers.empty()) {
         string best_id = "";
         double min_distance = -1;
@@ -158,8 +162,15 @@ bool GenRescue::Iterate()
           double py = point.y();
           
           double dist = hypot(px - current_x, py - current_y);
+          double dist_to_enemy = hypot(px - m_enemy_x, py - m_enemy_y);
+
+          bool enemy_known = (m_enemy_x != 0 || m_enemy_y != 0);
+          if (enemy_known && dist_to_enemy < dist && remaining_swimmers.size() > 1) {
+              continue;
+          }
+
           if(min_distance < 0 || dist < min_distance) {
-            min_distance = dist;
+            min_distance = dist; 
             best_id = id;
             best_x = px;
             best_y = py;
@@ -168,23 +179,44 @@ bool GenRescue::Iterate()
 
         if(best_id != "") {
           my_path.add_vertex(best_x, best_y);
-          current_x = best_x;
+          current_x = best_x; 
           current_y = best_y;
-          remaining_swimmers.erase(best_id);
+          remaining_swimmers.erase(best_id); 
         } else {
-          break; 
+          // 📌 ΔΙΟΡΘΩΘΗΚΕ: Αν όλοι είναι πιο κοντά στον εχθρό, 
+          // βρίσκουμε τον γεωμετρικά κοντινότερο σε εμάς και όχι τον αλφαβητικά πρώτο!
+          string nearest_id = "";
+          double nearest_dist = -1;
+          double nx = 0, ny = 0;
+
+          for(auto const& sp : remaining_swimmers) {
+            double d = hypot(sp.second.x() - current_x, sp.second.y() - current_y);
+            if(nearest_dist < 0 || d < nearest_dist) {
+              nearest_dist = d;
+              nearest_id = sp.first;
+              nx = sp.second.x();
+              ny = sp.second.y();
+            }
+          }
+
+          if(nearest_id != "") {
+            my_path.add_vertex(nx, ny);
+            current_x = nx;
+            current_y = ny;
+            remaining_swimmers.erase(nearest_id);
+          } else {
+            break;
+          }
         }
       }
 
       // Ενημέρωση του Helm
       if(my_path.size() > 0) {
         string update_str = "points=" + my_path.get_spec(); 
-        Notify("RESCUE_UPDATES", update_str);
-        first_run_done = true; // Κλειδώνουμε ότι στείλαμε την πρώτη διαδρομή!
+        Notify("RESCUE_UPDATES", update_str); 
+        m_first_run_done = true; // 📌 Χρήση της m_ μεταβλητής
       }
       
-      // Σημαντικό: Κατεβάζουμε το flag για να μην ξανατρέξει στο επόμενο Iterate 
-      // μέχρι να έρθει νέο μήνυμα αλλαγής!
       m_path_update_needed = false; 
   }
 
@@ -203,38 +235,33 @@ void GenRescue::RegisterVariables()
 {
   AppCastingMOOSApp::RegisterVariables();
   
-  // Το σκάφος πρέπει να ξέρει τη θέση του
   Register("NAV_X", 0);
   Register("NAV_Y", 0);
-  
-  // Το σκάφος πρέπει να ακούει τον Διαιτητή (uFldRescueMgr)
-  Register("SWIMMER_ALERT", 0); 
-  Register("FOUND_SWIMMER", 0); 
+  Register("SWIMMER_ALERT", 0); // [cite: 184, 333]
+  Register("FOUND_SWIMMER", 0); // [cite: 349]
+
+  // 📌 Γινόμαστε συνδρομητές στις live θέσεις των δύο πιθανών αντιπάλων
+  Register("NAV_X_BEN", 0);
+  Register("NAV_Y_BEN", 0);
+  Register("NAV_X_ABE", 0);
+  Register("NAV_Y_ABE", 0);
 }
+
 bool GenRescue::buildReport() 
 {
   m_msgs << "============================================" << endl;
   m_msgs << "pGenRescue " << m_host_community << " Status:" << endl;
   m_msgs << "============================================" << endl;
   
-  // Έλεγχος αν έχουμε λάβει στίγμα
   bool nav_ok = (m_nav_x != 0 || m_nav_y != 0);
   m_msgs << "NAV_X/Y Received:        " << (nav_ok ? "true" : "false") << endl;
-  
+  m_msgs << "Enemy X/Y Position:      [" << m_enemy_x << ", " << m_enemy_y << "]" << endl;
   m_msgs << endl;
   m_msgs << "Rescue Status              " << endl;
   m_msgs << "------------------------ " << endl;
-  
-  // Δείχνουμε πόσοι κολυμβητές υπάρχουν αυτή τη στιγμή
   m_msgs << "Active Swimmers in list: " << m_swimmers.size() << endl;
-
-  // --- ΝΕΕΣ ΠΡΟΣΘΗΚΕΣ ΓΙΑ ΚΑΛΥΤΕΡΟ APPCASTING ---
-  // Δείχνουμε πόσους έχουμε ήδη σώσει
   m_msgs << "Rescued Swimmers:        " << m_rescued_swimmers.size() << endl;
-  
-  // Δείχνουμε αν εκκρεμεί ανανέωση της διαδρομής (θα το βλέπεις να αναβοσβήνει "true" στιγμιαία)
   m_msgs << "Path Update Needed:      " << (m_path_update_needed ? "true" : "false") << endl;
-  // ----------------------------------------------
 
   return(true);
 }
